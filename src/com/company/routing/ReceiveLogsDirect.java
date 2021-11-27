334f202728d6a9c1d6f39d79dc1e0983cff85ba4
Georgii Vlasov <honest.niceman@gmail.com>
2021-11-27 19:01:42 +0400

    Routing

---
+++ src/com/company/routing/ReceiveLogsDirect.java
@@ -0,0 +1,42 @@
+package com.company.routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+public class ReceiveLogsDirect {
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+        String queueName = channel.queueDeclare().getQueue();
+
+        if (argv.length < 1) {
+            System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+            System.exit(1);
+        }
+
+        // we can pass multiple severity levels for one queue
+        for (String severity : argv) {
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+            System.out.println(" [*] Waiting for messages with severity = " + severity);
+        }
+        System.out.println("To exit press CTRL+C\n");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" +
+                    delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
+    }
+}
